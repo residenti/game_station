3d45acc5b32b5d00d3750a8eeb1969390bafec03
residenti <n.queue.r@gmail.com>
2021-10-17 17:06:22 +0900

    ユーザ招待URLのコピーボタンを実装した

--- app/javascript/controllers/club_controller.js
+++ app/javascript/controllers/club_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = [ 'invitationUrl' ]
+
   toggleInvitaionUrlModal() {
     document.getElementById('invitation-url-modal').classList.toggle('open-modal')
+  }
+  copyInvitaionUrl() {
+    this.invitationUrlTarget.select()
+    document.execCommand('copy')
   }
 }
